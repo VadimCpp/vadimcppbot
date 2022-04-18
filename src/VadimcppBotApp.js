5a04e1444cafa9556329b555d988a919f48fd9fd
Vadym Kaninskyi <vadim.cpp@gmail.com>
2022-04-18 12:57:10 +0200

    Remove unused code and pur stub

--- src/VadimcppBotApp.js
+++ src/VadimcppBotApp.js
@@ -1,83 +1,36 @@
 class VadimcppBotApp {
-
-  /**
-   * @public
-   */
   constructor() {
     console.log('');
-    console.log('[MyFirstBotApp]: Create Application...');
-
-    /**
-     * @type {Array}
-     * @private
-     */
-    this._botsBasedOnThisTemplate = [
-      '@gdgcensorbot',
-      '@events4friendsbot',
-      '@annagrabbot',
-      '@gdgdevbot',
-    ];
+    console.log('[VadimcppBotApp]: Create Application...');
   }
 
-  /**
-   * Main event handler
-   *
-   * @param {Object} msg
-   * @param {Object} bot
-   * @public
-   */
   handleMessage(msg, bot) {
-
-    //
-    // NOTE! view logs on your server
-    //
-    console.log('');
+    console.log(''); // NOTE! view logs on your server
     console.log(JSON.stringify(msg));
 
-    /**
-     * @type {string}
-     */
-    let messageText =
-      'Привет, ' + this._getName(msg) + "\n\n" +
-      'Это стартовый шаблон для разработки ботов на javascript с использованием <b>node-telegram-bot-api</b>.\n\n' +
-      'Примеры ботов:\n';
+    const isPrivateMsg = msg.chat.id > 0;
 
-    for (let i = 0; i < this._botsBasedOnThisTemplate.length; i++)
-      messageText += this._botsBasedOnThisTemplate[i] + ';\n';
+    if (isPrivateMsg) { // This is private chat
+      const messageText =
+        'Привет, ' + this._getName(msg) + "\n\n" +
+        'Я только учусь нормально общаться. Как дела?';
 
-    messageText += '\nНачните создание своего бота с изучения исходников на <a href="https://github.com/VadimCpp/myfirstbotbot">github</a>!';
-
-    bot.sendMessage(msg.chat.id, messageText, {
-      parse_mode: "HTML",
-      disable_web_page_preview: true,
-    });
+      bot.sendMessage(msg.chat.id, messageText, {
+        parse_mode: "HTML",
+        disable_web_page_preview: true,
+      });
+    } else {
+      // NOTE!
+      // This is public chat
+      // TODO:
+    }
   }
 
-  /**
-   * @param {Object} msg
-   * @return {string}
-   * @private
-   */
   _getName(msg) {
-    /**
-     * @type {string}
-     */
     let result = 'Без имени 👤';
-
-    /**
-     * @type {string}
-     */
-    let fname = msg.from.first_name;
-
-    /**
-     * @type {string}
-     */
-    let lname = msg.from.last_name;
-
-    /**
-     * @type {string}
-     */
-    let uname = msg.from.username;
+    const fname = msg.from.first_name;
+    const lname = msg.from.last_name;
+    const uname = msg.from.username;
 
     if (fname) {
       result = fname + ( lname ? ' ' + lname : '') ;
