4b248d662156c42d7e9b4bbf6e0b9aadbe388b07
Vadym Kaninskyi <vadim.cpp@gmail.com>
2022-04-18 12:45:16 +0200

    Reindent and rename files

--- bot.js
+++ bot.js
@@ -1,23 +1,23 @@
 const Bot = require('node-telegram-bot-api');
 const token = process.env.BOT_ACCESS_TOKEN;
 const isProduction = process.env.NODE_ENV === 'production';
-const MyFirstBotApp = require('./src/MyFirstBotApp.js');
+const VadimcppBotApp = require('./src/VadimcppBotApp.js');
 
-let myFirstBotApp = new MyFirstBotApp();
+let vadimcppBotApp = new VadimcppBotApp();
 let bot;
 
 if (isProduction) {
-    bot = new Bot(token);
-    bot.setWebHook(process.env.HEROKU_URL + bot.token);
+  bot = new Bot(token);
+  bot.setWebHook(process.env.HEROKU_URL + bot.token);
 } else {
-    bot = new Bot(token, { polling: true });
-    bot.deleteWebHook();
+  bot = new Bot(token, { polling: true });
+  bot.deleteWebHook();
 }
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.on('message', (msg) => {
-    myFirstBotApp.handleMessage(msg, bot);
+  vadimcppBotApp.handleMessage(msg, bot);
 });
 
 module.exports = bot;
---
+++ src/VadimcppBotApp.js
@@ -0,0 +1,92 @@
+class VadimcppBotApp {
+
+  /**
+   * @public
+   */
+  constructor() {
+    console.log('');
+    console.log('[MyFirstBotApp]: Create Application...');
+
+    /**
+     * @type {Array}
+     * @private
+     */
+    this._botsBasedOnThisTemplate = [
+      '@gdgcensorbot',
+      '@events4friendsbot',
+      '@annagrabbot',
+      '@gdgdevbot',
+    ];
+  }
+
+  /**
+   * Main event handler
+   *
+   * @param {Object} msg
+   * @param {Object} bot
+   * @public
+   */
+  handleMessage(msg, bot) {
+
+    //
+    // NOTE! view logs on your server
+    //
+    console.log('');
+    console.log(JSON.stringify(msg));
+
+    /**
+     * @type {string}
+     */
+    let messageText =
+      'Привет, ' + this._getName(msg) + "\n\n" +
+      'Это стартовый шаблон для разработки ботов на javascript с использованием <b>node-telegram-bot-api</b>.\n\n' +
+      'Примеры ботов:\n';
+
+    for (let i = 0; i < this._botsBasedOnThisTemplate.length; i++)
+      messageText += this._botsBasedOnThisTemplate[i] + ';\n';
+
+    messageText += '\nНачните создание своего бота с изучения исходников на <a href="https://github.com/VadimCpp/myfirstbotbot">github</a>!';
+
+    bot.sendMessage(msg.chat.id, messageText, {
+      parse_mode: "HTML",
+      disable_web_page_preview: true,
+    });
+  }
+
+  /**
+   * @param {Object} msg
+   * @return {string}
+   * @private
+   */
+  _getName(msg) {
+    /**
+     * @type {string}
+     */
+    let result = 'Без имени 👤';
+
+    /**
+     * @type {string}
+     */
+    let fname = msg.from.first_name;
+
+    /**
+     * @type {string}
+     */
+    let lname = msg.from.last_name;
+
+    /**
+     * @type {string}
+     */
+    let uname = msg.from.username;
+
+    if (fname) {
+      result = fname + ( lname ? ' ' + lname : '') ;
+    } else if (uname) {
+      result = uname;
+    }
+
+    return result;
+  }
+}
+
+module.exports = VadimcppBotApp;
