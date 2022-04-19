a3f19d036d1019c414643ea6a30a604096db8872
Vadym Kaninskyi <vadim.cpp@gmail.com>
2022-04-19 19:51:35 +0200

    Implement basic functionality to answer in private and public chats

--- src/VadimcppBotApp.js
+++ src/VadimcppBotApp.js
@@ -1,3 +1,78 @@
+const PUBLIC_VOCABULARY = [
+  {
+    questions: [
+      'Привет',
+      'Всем привет',
+    ],
+    answer: 'Привет 👋',
+  },
+];
+
+const PRIVATE_VOCABULARY = [
+  ...PUBLIC_VOCABULARY,
+  {
+    questions: [
+      'Привет',
+      'Приветствую',
+      'Доброе утро',
+      'Добрый день',
+      'Добрый вечер',
+      'Здорова',
+      'Вітаю',
+      'Добрий ранок',
+      'Добрий день',
+      'Добрий вечір',
+      "Здоровеньки були",
+    ],
+    answer: 'Давайте сразу к делу. Какой у Вас вопрос?',
+  },
+  {
+    questions: [
+      'Як справи?',
+      'Як життя?',
+      'Що робиш?',
+      'Що поробляєш?',
+      'Как дела?',
+      'Как жизнь?',
+      'Как поживаешь?',
+      'Что делаешь?',
+    ],
+    answer: 'Я работаю. Какой у Вас вопрос?',
+  },
+];
+
+// TODO: implement
+function extractQuestionOrGreeting(text) {
+  if (text.toLowerCase() === "привет" || text.toLowerCase() === "всем привет") {
+    return "привет";
+  }
+  return null;
+}
+
+function normalizeText(text) {
+  // remove spaces
+  let normalized = text.trim();
+
+  // put all to lowercase
+  normalized = normalized.toLowerCase();
+
+  // replace 'й' to 'и'
+  normalized = normalized.replace('й', 'и');
+
+  // replace 'ё' to 'е'
+  normalized = normalized.replace('ё', 'е');
+
+  // remove dots, commas, spaces etc.
+  normalized = normalized.replace('.', '');
+  normalized = normalized.replace(' ', '');
+  normalized = normalized.replace(',', '');
+  normalized = normalized.replace('\n', '');
+  normalized = normalized.replace('!', '');
+  normalized = normalized.replace('?', '');
+
+  return normalized;
+}
+
 class VadimcppBotApp {
   constructor() {
     console.log('');
@@ -9,19 +84,44 @@ class VadimcppBotApp {
     console.log(JSON.stringify(msg));
 
     const isPrivateMsg = msg.chat.id > 0;
-
-    if (isPrivateMsg) { // This is private chat
-      const messageText =
-        'Привет, ' + this._getName(msg) + "\n\n" +
-        'Я только учусь нормально общаться. Как дела?';
-
-      bot.sendMessage(msg.chat.id, messageText, {
+    if (isPrivateMsg) {
+      // In private chat bot react on every message
+      const answer = this._getAnswer(msg.text, PRIVATE_VOCABULARY);
+      const alternative = `${this._getName(msg)}, я не знаю, спроси пожалуйста в @borinorge. Я там учусь.`;
+      bot.sendMessage(msg.chat.id, answer || alternative, {
         parse_mode: "HTML",
         disable_web_page_preview: true,
-      });
-    } else { // This is public chat
-      console.log('TODO: implement');
+      }).then();
+    } else {
+      // In public groups bot react only to questions and greetings
+      const question = extractQuestionOrGreeting(msg.text || '');
+      if (question) {
+        const answer = this._getAnswer(msg.text, PUBLIC_VOCABULARY);
+        if (answer) {
+          bot.sendMessage(msg.chat.id, answer, {
+            parse_mode: "HTML",
+            disable_web_page_preview: true,
+            reply_to_message_id: msg.message_id,
+          }).then();
+        }
+      }
     }
+  }
+
+  _getAnswer(text, vocabulary) {
+    for (let i = 0; i < vocabulary.length; i++) {
+      const option = vocabulary[i];
+      for (let j = 0; j < option.questions.length; j++) {
+        const normalizedMsgText = normalizeText(text);
+        const normalizedQuestion = normalizeText(option.questions[j]);
+        if (normalizedMsgText === normalizedQuestion) {
+          console.log('Found answer for question: ', normalizedMsgText);
+          return option.answer;
+        }
+      }
+    }
+
+    return null;
   }
 
   _getName(msg) {
